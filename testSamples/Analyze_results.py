ec5375a8274673a81b877185cb00d340fa4612c5
Lygram <punchermacher@gmail.com>
2024-03-03 19:59:53 +0200

    commited script.

--- testSamples/Analyze_results.py
+++ testSamples/Analyze_results.py
@@ -15,6 +15,13 @@ plt.ylabel("Sensor return value")
 plt.xlabel("time")
 plt.savefig(f"{FILEPATH}/amg_avg.png")
 plt.close()
+avgData_dt = [avgData[i+1] - avgData[i] for i in range(len(avgData) -1)]
+plt.title("AMG average defferential data, indicative of sample rate")
+plt.ylabel("Sensor return value / time")
+plt.xlabel("time")
+plt.plot(avgData_dt)
+plt.savefig(f"{FILEPATH}/amg_avg_df_dt.png")
+plt.close()
 
 fig, ax = plt.subplots(8, 8, figsize=[28,18])
 fig.suptitle("AMG graphs for each pixel")
@@ -23,6 +30,7 @@ for i in range(8):
     for j in range(8):
         ax[j][i].plot(data[:, i, j])
 fig.savefig(f"{FILEPATH}/amg_pix.png")
+plt.close(fig)
 
 data = np.load(f"{FILEPATH}/ToF/latest/pkl_walk_2m_test2.pkl",
                allow_pickle=True)
@@ -35,7 +43,7 @@ plt.title("TOF test, mean result of all pixels in walk test")
 plt.ylabel("Sensor return value")
 plt.xlabel("time")
 plt.savefig(f"{FILEPATH}/tof_avg.png")
-plt.close(fig)
+plt.close()
 
 fig, ax = plt.subplots(8, 8, figsize=[28,18])
 fig.suptitle("TOF graphs for each pixel, walk test")
@@ -43,6 +51,7 @@ fig.tight_layout()
 for i in range(8):
     for j in range(8):
         ax[j][i].plot(data[:, i, j])
+        ax[j][i].set_ylim(0, 2000)
 fig.savefig(f"{FILEPATH}/tof_pix.png")
 plt.close(fig)
 
@@ -52,6 +61,13 @@ plt.title("TOF test, mean result of all pixels in hand test")
 plt.ylabel("Sensor return value")
 plt.xlabel("time")
 plt.savefig(f"{FILEPATH}/tof_hand_avg.png")
+avgData_dt = [avgData[i+1] - avgData[i] for i in range(len(avgData) -1)]
+plt.close()
+plt.title("TOF average defferential data, indicative of sample rate")
+plt.ylabel("Sensor return value / time")
+plt.xlabel("time")
+plt.plot(avgData_dt)
+plt.savefig(f"{FILEPATH}/tof_hand_avg_df_dt.png")
 plt.close()
 
 fig, ax = plt.subplots(8, 8, figsize=[28,18])
@@ -60,6 +76,7 @@ fig.tight_layout()
 for i in range(8):
     for j in range(8):
         ax[j][i].plot(data2[:, i, j])
+        #ax[j][i].set_ylim(0, 2000)
 fig.savefig(f"{FILEPATH}/tof_hand_pix.png")
 plt.close(fig)
 
